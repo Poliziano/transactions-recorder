f4a0bbbbb5da7ce3ffc9080033ede379952ba97f
jordanrowe <jordanneilrowe@live.co.uk>
2022-05-02 17:03:39 +0100

    feat: create lambda-backed REST API

--- apps/backend/lib/stack.js
+++ apps/backend/lib/stack.js
@@ -1,17 +1,22 @@
 const { Stack } = require("aws-cdk-lib");
-const lambda = require("aws-cdk-lib/aws-lambda");
 const { NodejsFunction } = require("aws-cdk-lib/aws-lambda-nodejs");
+const lambda = require("aws-cdk-lib/aws-lambda");
+const apigateway = require("aws-cdk-lib/aws-apigateway");
 const path = require("path");
 
 class TransactionsStack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    new NodejsFunction(this, "TransactionGet", {
+    const lambdaTransactionGet = new NodejsFunction(this, "TransactionGet", {
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: "handler",
       entry: path.resolve(__dirname, "lambda", "transaction-get.js"),
     });
+
+    const api = new apigateway.RestApi(this, "transactions");
+    const integration = new apigateway.LambdaIntegration(lambdaTransactionGet)
+    api.root.addMethod("GET", integration);
   }
 }
 
