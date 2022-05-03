a14c57c3aca5bc2e3daf2ced8943c9bc80da8e86
jordanrowe <jordanneilrowe@live.co.uk>
2022-05-03 19:52:33 +0100

    fix: failing tests

--- apps/backend/lib/test/transaction-create.test.js
+++ apps/backend/lib/test/transaction-create.test.js
@@ -4,7 +4,12 @@ const { handler } = require("../lambda/transaction-create");
 test("transaction-create should throw when invalid payload", async () => {
   const result = await handler();
   expect(result).toStrictEqual({
-    statusCode: 400
+    statusCode: 400,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 });
 
@@ -17,7 +22,12 @@ test("transaction-create should throw when partial payload", async () => {
   });
 
   expect(result).toStrictEqual({
-    statusCode: 400
+    statusCode: 400,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 });
 
@@ -26,15 +36,20 @@ test("transaction-create should not thrown when valid payload", async () => {
     body: JSON.stringify({
       date: new Date(2021, 1, 1),
       name: "McDonalds",
-      amount: 12.50,
+      amount: 12.5,
       type: "expenditure",
     }),
     pathParameters: {
-      userId: "user_id"
-    }
+      userId: "user_id",
+    },
   });
 
   expect(result).toStrictEqual({
-    statusCode: 200
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 });
