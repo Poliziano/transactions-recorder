fcbba333288a23e9baa23b221d6957264a45decf
jordanrowe <jordanneilrowe@live.co.uk>
2022-05-03 20:09:48 +0100

    feat: adds transactions app specific lambda

--- apps/backend/lib/stack.js
+++ apps/backend/lib/stack.js
@@ -12,26 +12,26 @@ class TransactionsStack extends Stack {
     const transactionsTable = new dynamodb.Table(this, "Transactions", {
       partitionKey: {
         name: "PK",
-        type: dynamodb.AttributeType.STRING
+        type: dynamodb.AttributeType.STRING,
       },
       sortKey: {
         name: "SK",
-        type: dynamodb.AttributeType.STRING
+        type: dynamodb.AttributeType.STRING,
       },
-      tableName: "Transactions"
+      tableName: "Transactions",
     });
 
-    const lambdaTransactionGet = new NodejsFunction(this, "TransactionGet", {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: "handler",
-      entry: path.resolve(__dirname, "lambda", "transaction-get.js"),
-    });
+    const lambdaTransactionGet = new TransactionsNodejsFunction(
+      this,
+      "TransactionGet",
+      "transaction-get.js"
+    );
 
-    const lambdaTransactionCreate = new NodejsFunction(this, "TransactionCreate", {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: "handler",
-      entry: path.resolve(__dirname, "lambda", "transaction-create.js"),
-    });
+    const lambdaTransactionCreate = new TransactionsNodejsFunction(
+      this,
+      "TransactionCreate",
+      "transaction-create.js"
+    );
 
     transactionsTable.grantReadData(lambdaTransactionGet);
     transactionsTable.grantWriteData(lambdaTransactionCreate);
@@ -41,9 +41,25 @@ class TransactionsStack extends Stack {
       .addResource("users")
       .addResource("{userId}")
       .addResource("transactions");
-    
-    transactionsResource.addMethod("GET", new apigateway.LambdaIntegration(lambdaTransactionGet));
-    transactionsResource.addMethod("POST", new apigateway.LambdaIntegration(lambdaTransactionCreate));
+
+    transactionsResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(lambdaTransactionGet)
+    );
+    transactionsResource.addMethod(
+      "POST",
+      new apigateway.LambdaIntegration(lambdaTransactionCreate)
+    );
+  }
+}
+
+class TransactionsNodejsFunction extends NodejsFunction {
+  constructor(scope, id, filename) {
+    super(scope, id, {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "handler",
+      entry: path.resolve(__dirname, "lambda", filename),
+    });
   }
 }
 
