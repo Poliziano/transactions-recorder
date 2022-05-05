eeea4c0a43541fdafd638b3d0c0a2f1eff72044b
jordanrowe <jordanneilrowe@live.co.uk>
2022-05-05 22:11:29 +0100

    feat: log result of getting transactions

--- apps/frontend/src/routes/index.svelte
+++ apps/frontend/src/routes/index.svelte
@@ -1,18 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <script lang="ts">
-  (async function() {
-    const url = "https://w3nezq09u5.execute-api.eu-west-2.amazonaws.com/prod/users/abc/transactions";
-    const transactions = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify({
-        "date": "2020-01-01",
-        "name": "McDonalds",
-        "amount": 12.50,
-        "type": "expenditure"
-      })
-    });
-    console.log(transactions);
-  })();
+	let result = 'pending';
+
+	(async function () {
+		const url =
+			'https://w3nezq09u5.execute-api.eu-west-2.amazonaws.com/prod/users/abc/transactions';
+		const response = await fetch(url, {
+			method: 'GET'
+		});
+
+		if (response.ok) {
+			const transactions = await response.json();
+			result = JSON.stringify(transactions, null, 2);
+		} else {
+			result = 'Failed to get';
+		}
+	})();
 </script>
+
+<p>{result}</p>
