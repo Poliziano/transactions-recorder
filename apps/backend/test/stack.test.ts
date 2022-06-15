5b78812eed90a4dfbb9511027f7ba641d6c29ea8
jordanrowe <jordanneilrowe@live.co.uk>
2022-06-15 16:20:11 +0100

    feat: adds option to disable stack bundling

--- apps/backend/test/stack.test.ts
+++ apps/backend/test/stack.test.ts
@@ -1,12 +1,17 @@
 import { beforeAll, expect, test } from "@jest/globals";
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
+import { BUNDLING_STACKS } from "aws-cdk-lib/cx-api";
 import { TransactionsStack } from "../lib/stack";
 
 let template: any;
 
 beforeAll(() => {
-  const app = new cdk.App();
+  const app = new cdk.App({
+    context: {
+      [BUNDLING_STACKS]: Boolean(process.env.DISABLE_CDK_BUNDLING) ? [] : ["*"],
+    },
+  });
   const stack = new TransactionsStack(app, "test");
   template = Template.fromStack(stack);
 });
