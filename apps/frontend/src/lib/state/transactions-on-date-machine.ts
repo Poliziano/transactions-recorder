d24eb98f1ce1cefc5908a47fcd20c89ecd2a6c34
jordanrowe <jordanneilrowe@live.co.uk>
2022-07-13 21:41:02 +0100

    feat: pass through open event

--- apps/frontend/src/lib/state/transactions-on-date-machine.ts
+++ apps/frontend/src/lib/state/transactions-on-date-machine.ts
@@ -1,11 +1,19 @@
 import type { TransactionEntity } from "$lib/api/transaction";
-import { assign, createMachine, spawn, type ActorRefFrom } from "xstate";
+import type { TransactionFormParams } from "$lib/transaction-form";
+import {
+  assign,
+  createMachine,
+  sendParent,
+  spawn,
+  type ActorRefFrom,
+} from "xstate";
 import { createTransactionMachine } from "./transaction-machine";
 import { fetchTransactionsForDate } from "./transactions.service";
 
 /** @xstate-layout N4IgpgJg5mDOIC5QBcBOBDAdrdBjZAlgPbYC0JpE6yYAdABYEQGZQAqG2ehJsAxADEAomwDCACQD6bAEoBBAHIBlOaLYBJAPLLEoAA5FYBHpl0gAHogBMAdgAMtAMwBWAJyOALI6vuPV5y4ANCAAnojOdja0rjGukY4AHAlWAIwekQC+GcFoWDj4xGQUVDS0AGZgyLiMrBx53IX8ECR0LABuRADWdBVV9HVcBbxmBkYmZpYINjG0ASlWAGyLyUsxwWEIHkm0HrELNguOkXYLWTmc+Saw5JiU1HTMsHoANughLOwXDbx8ogAymiUQmk8mUqg02iUI0MxkKE0QCTsHmiPhsbisPiWNgS60QLgW0ViNg8hw8xLsjjOIFygyuNzuND4mgACkIFCDFCo1FplJIBJoZABZaFjOFICzhI60KwJVwpTx2VI2FILXEIFIJZyE2I6nU2Kk0y6NeklMB8OTM1kKAAiHLB3O0IthJHhCGcuxRKVcVi8thS8psatcCW1ut1+uy1K+QyKt1NfBkQkFmgAasDZJzwTyneNxZN3SG7K4DgkFs45gtXGr5llI5giBA4GZDd9Ywy6IxmLVo1cc2LQJMyWqXCHHGP-GXiQlPAcDT3jcV7uVKtUPgMjcNxaNnaY84hdg5va5K-Z-cWfWqyQTdjF9odjm5nHP6jHrovSo8Xm81-PN-oYbmA6ICkipakkPjyseIGJFWoR4gsDjTmORaHM49h2KWz60gucb3H2Lp7ggViOGqMoeLWGRAA */
 const machine = createMachine(
   {
+    id: "transactions-on-date",
     tsTypes: {} as import("./transactions-on-date-machine.typegen").Typegen0,
     schema: {
       context: {} as {
@@ -17,14 +25,13 @@ const machine = createMachine(
         >;
       },
       events: {} as
-        | { type: "OPEN_TRANSACTIONS_FORM" }
+        | { type: "OPEN_TRANSACTIONS_FORM"; data: TransactionFormParams }
         | { type: "APPEND_TRANSACTION"; data: TransactionEntity }
         | { type: "REMOVE_TRANSACTION"; data: TransactionEntity["uuid"] }
         | { type: "FETCH_TRANSACTIONS" }
         | { type: "done.invoke.fetchTransactions"; data: TransactionEntity[] }
         | { type: "CLOSE_TRANSACTIONS" },
     },
-    id: "transactions-on-date",
     initial: "hidingTransactions",
     states: {
       hidingTransactions: {
@@ -118,7 +125,7 @@ const machine = createMachine(
           );
         },
       }),
-      notifyOpenForm: () => {},
+      notifyOpenForm: sendParent((_context, event) => event),
     },
   }
 );
